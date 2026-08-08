7621d102f02f687c290ed671046bf3348d5b269e
Teragen <67575979+Teragenic@users.noreply.github.com>
2026-08-08 15:53:16 +0800

    Azure Peak - >Rotwood Vale

--- tgui/packages/tgui/interfaces/MigrantPanel.tsx
+++ tgui/packages/tgui/interfaces/MigrantPanel.tsx
@@ -362,7 +362,7 @@ export const MigrantPanel = () => {
         <div style={pageStyle}>
           <div style={titleStyle}>Find a Purpose</div>
           <div style={subtitleStyle}>
-            The mist parts, and travellers find their way to Azure Peak.
+            The mist parts, and travellers find their way to Rotwood Vale.
           </div>
 
           <div
